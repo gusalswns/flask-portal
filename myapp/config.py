54bdab3776a9e2ee768f1dd2cc869a98d5fd1223
gusalswns <hmj3625000@gmail.com>
2025-07-17 10:05:30 +0900

    Delete myapp directory

--- myapp/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-SECRET_KEY = "supersecretkey"
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
-MAX_CONTENT_LENGTH = 100 * 1024 * 1024  # 100MB
-DATABASE = os.path.join(os.getcwd(), 'users.db')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'avi', 'pdf', 'txt', 'zip'}
